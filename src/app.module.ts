8e802bba71467aa5a222cdbc8a7828bb3d07afe2
benchik <wygndo@gmail.com>
2025-10-15 19:40:31 +0300

    off queue module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigAppModule } from './config/config.module';
 import { BitrixModule } from './modules/bitirx/bitrix.module';
-import { QueueModule } from './modules/queue/queue.module';
 import { RedisModule } from './modules/redis/redis.module';
 
 @Module({
-  imports: [ConfigAppModule, BitrixModule, QueueModule, RedisModule],
+  imports: [ConfigAppModule, BitrixModule, RedisModule],
   controllers: [],
   providers: [],
 })
--- src/common/config/redis.config.ts
+++ src/common/config/redis.config.ts
@@ -2,7 +2,6 @@ import { RedisOptions } from 'bullmq';
 
 export default (): { redisConfig: RedisOptions } => ({
   redisConfig: {
-    url: process.env.REDIS_URL,
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT ? +process.env.REDIS_PORT : 6379,
     username: process.env.REDIS_USER,
