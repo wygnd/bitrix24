8cbc067f9d7306bbdc9a18d1c7c6fc8f044ae611
wygnd <wygndo@gmail.com>
2025-10-22 13:45:35 +0300

    add request-response middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigAppModule } from './config/config.module';
 import { RedisModule } from './modules/redis/redis.module';
 import { AppHttpModule } from './modules/http/http.module';
 import { BitrixModule } from './modules/bitirx/bitrix.module';
 import { ApplicationLoggerModule } from '@/modules/logger/logger.module';
+import { HttpLoggerMiddleware } from '@/common/middlewares/http-logger.middleware';
 
 @Module({
-  imports: [
-    ConfigAppModule,
-    RedisModule,
-    AppHttpModule,
-    BitrixModule,
-  ],
+  imports: [ConfigAppModule, RedisModule, AppHttpModule, BitrixModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpLoggerMiddleware).forRoutes('*');
+  }
+}
