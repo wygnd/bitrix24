c39ce83f63ca5cfaaa3ec39a532b1cf806631d33
wygnd <wygndo@gmail.com>
2025-10-29 13:01:20 +0300

    add redirect on api url

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { RedisModule } from './modules/redis/redis.module';
 import { BitrixModule } from './modules/bitirx/bitrix.module';
 import { HttpLoggerMiddleware } from '@/common/middlewares/http-logger.middleware';
 import { HeadHunterModule } from '@/modules/headhunter/headhunter.module';
+import { AppController } from '@/app.controller';
 
 @Module({
   imports: [ConfigAppModule, RedisModule, BitrixModule, HeadHunterModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule implements NestModule {
