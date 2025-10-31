192ba8918a46eaf8427f496d1af8c6048035b6d0
wygnd <wygndo@gmail.com>
2025-10-31 16:13:40 +0300

    fix bitrix placement guard

--- src/modules/bitirx/guards/bitrix-widget.guard.ts
+++ src/modules/bitirx/guards/bitrix-widget.guard.ts
@@ -5,8 +5,8 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { BitrixService } from '@/modules/bitirx/bitrix.service';
-import { Request, Response } from 'express';
-import { PlacementRequestDto } from '@/modules/bitirx/modules/placement/dtos/placement-request.dto';
+import { Request } from 'express';
+import { PlacementBodyRequestDto } from '@/modules/bitirx/modules/placement/dtos/placement-request.dto';
 
 @Injectable()
 export class BitrixPlacementGuard implements CanActivate {
@@ -15,7 +15,7 @@ export class BitrixPlacementGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest<Request>();
 
-    const body: PlacementRequestDto = request.body;
+    const body: PlacementBodyRequestDto = request.body;
 
     const memberIdFromRequest = body?.member_id;
     const memberId = this.bitrixService.WEBHOOK_INCOMING_TOKEN;
