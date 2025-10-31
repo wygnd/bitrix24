f5f56da20079b1cc90f13eed20cab80a19fe43c6
benchik <wygndo@gmail.com>
2025-10-31 06:31:23 +0300

    change placement endpoints

--- src/modules/bitirx/modules/placement/placement.controller.ts
+++ src/modules/bitirx/modules/placement/placement.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { ApiHeader, ApiTags } from '@nestjs/swagger';
 import { B24ApiTags } from '@/modules/bitirx/interfaces/bitrix-api.interface';
 import { PlacementRequestDto } from '@/modules/bitirx/modules/placement/dtos/placement-request.dto';
 import type { Response } from 'express';
+import { BitrixPlacementService } from '@/modules/bitirx/modules/placement/bitrix-placement.service';
+import { PlacementBindDto } from '@/modules/bitirx/modules/placement/dtos/placement-bind.dto';
+import { PlacementUnbindDto } from '@/modules/bitirx/modules/placement/dtos/placement-unbind.dto';
+import { AuthGuard } from '@/common/guards/auth.guard';
 
 @ApiTags(B24ApiTags.PLACEMENT)
 @Controller('placement')
 export class BitrixPlacementController {
-  constructor() {}
+  constructor(
+    private readonly bitrixPlacementService: BitrixPlacementService,
+  ) {}
 
   @Post('/crm/deal/detail-tab')
   async handleCrmDealDetailTab(
@@ -15,5 +21,29 @@ export class BitrixPlacementController {
     @Res() res: Response,
   ) {
     res.redirect('https://bitrix-hr-app-production.up.railway.app');
+  }
+
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Authorization header',
+    required: true,
+    example: 'bga token',
+  })
+  @UseGuards(AuthGuard)
+  @Post('/placement/bind')
+  async bindWidget(@Body() fields: PlacementBindDto) {
+    return this.bitrixPlacementService.bind(fields);
+  }
+
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Authorization header',
+    required: true,
+    example: 'bga token',
+  })
+  @UseGuards(AuthGuard)
+  @Post('/placement/unbind')
+  async unbindWidget(@Body() fields: PlacementUnbindDto) {
+    return this.bitrixPlacementService.unbind(fields);
   }
 }
