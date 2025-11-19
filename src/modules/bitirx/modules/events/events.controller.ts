39a54aa6a10ff9262fae8adef72bc3576ae92fdf
wygnd <wygndo@gmail.com>
2025-11-19 10:46:03 +0300

    add log on update task

--- src/modules/bitirx/modules/events/events.controller.ts
+++ src/modules/bitirx/modules/events/events.controller.ts
@@ -15,11 +15,17 @@ import { EventAddDto } from '@/modules/bitirx/modules/events/dtos/event-add.dto'
 import { BitrixEventGuard } from '@/modules/bitirx/guards/bitrix-event.guard';
 import { EventHandleUpdateTaskDto } from '@/modules/bitirx/modules/events/dtos/event-task-update.dto';
 import express from 'express';
+import { BitrixImBotService } from '@/modules/bitirx/modules/imbot/imbot.service';
+import { BitrixService } from '@/modules/bitirx/bitrix.service';
 
 @ApiTags(B24ApiTags.EVENTS)
 @Controller('events')
 export class BitrixEventsController {
-  constructor(private readonly eventsService: BitrixEventService) {}
+  constructor(
+    private readonly eventsService: BitrixEventService,
+    private readonly botService: BitrixImBotService,
+    private readonly bitrixService: BitrixService,
+  ) {}
 
   @UseGuards(AuthGuard)
   @Post('/add')
@@ -39,6 +45,12 @@ export class BitrixEventsController {
       await this.eventsService.handleTaskUpdate(fields);
 
     if (!resultHandlingTask) status = HttpStatus.ACCEPTED;
+
+    if (resultHandlingTask)
+      this.botService.sendMessage({
+        DIALOG_ID: this.bitrixService.TEST_CHAT_ID,
+        MESSAGE: 'Изменение задчи[br]' + JSON.stringify(fields),
+      });
 
     res.status(status).json(resultHandlingTask);
   }
