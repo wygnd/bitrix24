da16a7d0d524016cbb35f886a59e2f836c49f94c
benchik <wygndo@gmail.com>
2025-11-23 13:51:28 +0300

    fix deploy error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,50 +1,12 @@
-import {
-  Controller,
-  Get,
-  Header,
-  NotFoundException,
-  Redirect,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
-import { createReadStream } from 'node:fs';
-import { join } from 'path';
-import type { Response } from 'express';
-import { existsSync } from 'fs';
-import { AuthGuard } from '@/common/guards/auth.guard';
-import { QueueService } from '@/modules/queue/queue.service';
 
 @ApiExcludeController()
 @Controller()
 export class AppController {
-  constructor(private readonly bitrixQueue: QueueService) {}
+  constructor() {}
 
   @Get()
   @Redirect('/api', 301)
   async main() {}
-
-  @UseGuards(AuthGuard)
-  @Get('/dashboard')
-  @Header('Content-Type', 'text/html')
-  async dashboard(@Res() response: Response) {
-    const filepath = join(__dirname, '..', '/static/dashboard.html');
-
-    if (!existsSync(filepath)) throw new NotFoundException();
-
-    const file = createReadStream(filepath);
-
-    file.pipe(response);
-  }
-
-  @Get('/test')
-  async test() {
-    let i = 0;
-    this.bitrixQueue.addTaskTest(i.toString());
-    // while (i < 10) {
-    //   i++;
-    // }
-
-    return true;
-  }
 }
