a0b77454d7bc880e04b4f3f5bd30599a69b4a4b3
wygnd <wygndo@gmail.com>
2025-11-24 11:10:09 +0300

    remove queue service from app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
-import { QueueService } from '@/modules/queue/queue.service';
 
 @ApiExcludeController()
 @Controller()
 export class AppController {
-  constructor(private readonly queueService: QueueService) {}
+  constructor() {}
 
   @Get()
   @Redirect('/api', 301)
   async main() {}
-
-  @Get('/test')
-  async testQueue() {
-    return this.queueService.addTaskTest('pisapopa');
-  }
 }
