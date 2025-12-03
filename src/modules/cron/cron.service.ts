9a2aa82cce3124a42f9097ae3b19245d32923997
wygnd <wygndo@gmail.com>
2025-12-03 12:21:13 +0300

    comment cron schedule

--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -10,7 +10,7 @@ export class CronService {
    * Add task on notify ksenya about uploading calling from megafon.
    * Task running every work day at 9:10 am
    */
-  @Cron('* 10 9 * * 1-5')
+  // @Cron('* 10 9 * * 1-5')
   async handleCronNotifyAboutNeedLoadCallingFromMegafon() {
     this.bitrixService.callBatch({
       send_message: {
