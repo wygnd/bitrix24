59056eaace7f9157f601176135b614d836cee3d4
wygnd <wygndo@gmail.com>
2025-12-04 09:00:08 +0300

    set cron timeson

--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -10,7 +10,9 @@ export class CronService {
    * Add task on notify ksenya about uploading calling from megafon.
    * Task running every work day at 9:10 am
    */
-  @Cron('* 10 9 * * 1-5')
+  @Cron('* 10 9 * * 1-5', {
+    timeZone: 'Europe/Moscow',
+  })
   async handleCronNotifyAboutNeedLoadCallingFromMegafon() {
     this.bitrixService.callBatch({
       send_message: {
