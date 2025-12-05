bd77c2cdc087606b36f231074d4ff60c05504298
wygnd <wygndo@gmail.com>
2025-12-05 12:21:50 +0300

    remove test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
-import { QueueHeavyService } from '@/modules/queue/queue-heavy.service';
 
 @ApiExcludeController()
 @Controller()
 export class AppController {
-  constructor(private readonly queueHeavyService: QueueHeavyService) {}
+  constructor() {}
 
   @Get()
   @Redirect('/api', 301)
   async main() {}
-
-  @Get('/test')
-  async testHook() {
-    return this.queueHeavyService.getJobList();
-  }
 }
