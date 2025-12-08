6fc3d6f05dcf59b1af5bef37216e172bac7d84e9
wygnd <wygndo@gmail.com>
2025-12-08 14:41:48 +0300

    add temporary create tokens

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import { Body, Controller, Get, Redirect } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
+import { TokensService } from '@/modules/tokens/tokens.service';
+import { TokensCreationalAttributes } from '@/modules/tokens/interfaces/tokens-attributes.interface';
+import { TokensServices } from '@/modules/tokens/interfaces/tokens-serivces.interface';
+import { IsIn, IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+class CreateTokensDto implements TokensCreationalAttributes {
+  @IsNotEmpty()
+  @IsString()
+  accessToken: string;
+  @IsNotEmpty()
+  @IsString()
+  refreshToken: string;
+  @IsNotEmpty()
+  @IsIn(Object.values(TokensServices))
+  service: TokensServices;
+  @IsNotEmpty()
+  @IsInt()
+  expires: number;
+  @IsOptional()
+  @IsString()
+  notice?: string;
+}
 
 @ApiExcludeController()
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(private readonly tokensService: TokensService) {}
 
   @Get()
   @Redirect('/api', 301)
   async main() {}
+
+  @Get('/create-tokens')
+  async creteTokens(@Body() fields: CreateTokensDto) {
+    return this.tokensService.createToken(fields);
+  }
 }
