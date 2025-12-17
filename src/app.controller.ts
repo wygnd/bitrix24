68b282d266251a354a547a61666b2739bac99c80
wygnd <wygndo@gmail.com>
2025-12-17 11:18:26 +0300

    remove test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,10 @@
-import { BadRequestException, Controller, Get, Redirect } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
-import { TelphinService } from '@/modules/telphin/telphin.service';
 
 @ApiExcludeController()
 @Controller()
 export class AppController {
-  constructor(private readonly telphinService: TelphinService) {}
+  constructor() {}
 
   @Get()
   @Redirect('/api', 301)
@@ -14,15 +13,5 @@ export class AppController {
   @Get('/health')
   async getStatus() {
     return { status: 'ok' };
-  }
-
-  @Get('/test')
-  async testHook() {
-    const telphinAppInfo = await this.telphinService.getUserInfo();
-
-    if (!telphinAppInfo)
-      throw new BadRequestException('Invalid get telphin application info');
-
-    return this.telphinService.getClientExtensionByBitrixUserId(telphinAppInfo.client_id, '522');
   }
 }
