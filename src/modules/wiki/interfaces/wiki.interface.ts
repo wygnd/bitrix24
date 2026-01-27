932c85983918cd251373d6df381906e286d127be
wygnd <wygndo@gmail.com>
2026-01-27 09:22:04 +0300

    fix handle receive payment after push button from bitrix: add new action type

--- src/modules/wiki/interfaces/wiki.interface.ts
+++ src/modules/wiki/interfaces/wiki.interface.ts
@@ -1,1 +1,4 @@
-export type WikiOldActions = 'gft_log_user_money' | 'gft_setup_payment_group';
+export type WikiOldActions =
+  | 'gft_log_budget_money'
+  | 'gft_log_user_money'
+  | 'gft_setup_payment_group';
