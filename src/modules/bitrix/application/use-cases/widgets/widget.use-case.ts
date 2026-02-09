cc86249e2a2ccefc5825c9cb4adc90ede55f1f67
wygnd <wygndo@gmail.com>
2026-02-09 13:17:11 +0300

    remove logs from get data for background worker when init call card

--- src/modules/bitrix/application/use-cases/widgets/widget.use-case.ts
+++ src/modules/bitrix/application/use-cases/widgets/widget.use-case.ts
@@ -45,8 +45,6 @@ export class BitrixWidgetUseCase {
   async getDataForCallOnBackgroundWorker(phone: string) {
     if (!phone) throw new BadRequestException('Invalid phone number');
 
-    this.logger.log(`Check phone: ${phone}`)
-
     const clientPhone =
       phone[0] === '8'
         ? phone.replace('8', '+7')
