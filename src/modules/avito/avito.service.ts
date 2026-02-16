9cb9de2792fe46c869007078e1750f84be7b0997
wygnd <wygndo@gmail.com>
2026-02-16 14:54:35 +0300

    add logs on send request to avito

--- src/modules/avito/avito.service.ts
+++ src/modules/avito/avito.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { AvitoApiService } from '@/modules/avito/avito-api.service';
 import { AvitoRejectDistributeDealByAiRequest } from '@/modules/avito/interfaces/avito-reject-distribute-deal-by-ai.interface';
+import { WinstonLogger } from '@/config/winston.logger';
 
 @Injectable()
 export class AvitoService {
+  private readonly logger = new WinstonLogger(
+    AvitoService.name,
+    'avito'.split(':'),
+  );
+
   constructor(private readonly avitoApiService: AvitoApiService) {}
 
   /**
@@ -15,12 +21,26 @@ export class AvitoService {
     phone: string,
     message: string = 'Лид, созданный AI отклонен для обработки',
   ) {
-    return this.avitoApiService.post<AvitoRejectDistributeDealByAiRequest>(
-      '/messenger-db/warnings/by-phone',
-      {
-        phone,
-        message,
-      },
-    );
+    try {
+      const response =
+        await this.avitoApiService.post<AvitoRejectDistributeDealByAiRequest>(
+          '/messenger-db/warnings/by-phone',
+          {
+            phone,
+            message,
+          },
+        );
+      this.logger.debug({
+        handler: this.rejectDistributeLeadByAi.name,
+        response,
+      });
+      return response;
+    } catch (error) {
+      this.logger.error({
+        handler: this.rejectDistributeLeadByAi.name,
+        error,
+      });
+      throw error;
+    }
   }
 }
