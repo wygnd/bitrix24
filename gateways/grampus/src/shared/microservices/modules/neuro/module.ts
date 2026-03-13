ea209004550deadd112bb8c7b18ebbe6de333735
wygnd <wygndo@gmail.com>
2026-03-13 10:56:55 +0300

    fix: moved module

--- gateways/grampus/src/shared/microservices/modules/neuro/module.ts
+++ gateways/grampus/src/shared/microservices/modules/neuro/module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
 import { NeuroService } from './services/service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ConfigService } from '@nestjs/config';
-import { MICROSERVICES } from '@/shared/microservices/constants/constants';
 
 @Module({
   imports: [
